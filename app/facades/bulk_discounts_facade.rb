bfab96ee4125d4e62076e035543fb6acb8f9fb67
Gregory Fischer <gwhoisj@gmail.com>
2021-11-14 12:22:17 -0500

    Feat: add service to facade

--- app/facades/bulk_discounts_facade.rb
+++ app/facades/bulk_discounts_facade.rb
@@ -5,4 +5,16 @@ class BulkDiscountsFacade
     @merchant = merchant
     @bulk_discounts = merchant.bulk_discounts
   end
+
+  def service
+    HolidayService.new
+  end
+
+  def names
+    service.holiday_names
+  end
+
+  def dates
+    service.holiday_dates
+  end
 end
