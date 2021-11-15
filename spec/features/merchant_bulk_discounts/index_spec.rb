6532e217a44b9cc0f4617b54e9667ee75e5214b1
Gregory Fischer <gwhoisj@gmail.com>
2021-11-15 12:57:40 -0500

    Feat: add creat feature

--- spec/features/merchant_bulk_discounts/index_spec.rb
+++ spec/features/merchant_bulk_discounts/index_spec.rb
@@ -35,6 +35,30 @@ RSpec.describe 'bulk discount index' do
     click_link("New Discount")
 
     expect(current_path).to eq "/merchants/#{@merchant.id}/bulk_discounts/new"
+
+    fill_in "Item Threshold", with: 15
+    fill_in 'Discount Percentage', with: 10
+    click_button 'Save'
+
+    id = BulkDiscount.last.id
+
+    within("#discount-#{id}") do
+      expect(page).to have_content "Discount: 10% Threshold: 15"
+    end
+  end
+
+  it "doesn't allow creating an invalid discount" do
+    visit merchant_bulk_discounts_path(@merchant)
+
+    click_link("New Discount")
+
+    expect(current_path).to eq "/merchants/#{@merchant.id}/bulk_discounts/new"
+
+    fill_in "Item Threshold", with: 15
+    fill_in 'Discount Percentage', with: 101
+    click_button 'Save'
+
+    expect(page).to have_content "Error - could not create bulk discount"
   end
 
   it 'lists the next three holidays' do
